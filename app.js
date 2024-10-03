ece0bfa8a2a6764db3225615685cde21711a90b2
Paulo Calueto <paulo.calueto.francisco@gmail.com>
2024-10-03 06:30:15 +0100

    new update

--- app.js
+++ app.js
@@ -24,7 +24,11 @@ const searchWeather = ()=>{
             city.querySelector('figcaption').innerText = data.name;
             city.querySelector('img').src = 'https://flagsapi.com/'+data.sys.country+'/shiny/32.png';
             temperature.querySelector('img').src = 'http://openweathermap.org/img/wn/'+data.weather[0].icon+'@4x.png';
-
+            temperature.querySelector('figcaption span').innerText = data.main.temp;
+            description.innerText = data.weather[0].description;
+            cloud.innerText =data.clouds.all;
+            humidity.innerText = data.main.humidity;
+            pressure.innerText = data.main.pressure;
         }
     });
 }
